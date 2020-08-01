cb66d2e722443fa71bfdd6884ce4330b80faab3d
sshukhratov <68948399+sshukhratov@users.noreply.github.com>
2020-08-01 15:35:08 -0500

    Update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -4,5 +4,10 @@ public class HappyB20 {
         System.out.println(name);
         
         System.out.println("Hello hello how are you");
+        
+        for (int i = 0; i <= 5; i ++i){
+            System.out.println("*****");
+            
+        }
     }
 }
