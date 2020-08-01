0335171a7da24380e01ae472f5a5d1f2bce6d458
sshukhratov <68948399+sshukhratov@users.noreply.github.com>
2020-08-01 15:11:33 -0500

    Update HappyB20.java

--- src/HappyB20.java
+++ src/HappyB20.java
@@ -2,5 +2,7 @@ public class HappyB20 {
     public static void main(String[] args) {
         String name = "Batch20";
         System.out.println(name);
+        
+        System.out.println("Hello hello");
     }
 }
